5d539c957043c98d418c845cb00dd938efac6fc9
naderkamelaponar <nader.kamel.the.programmer@gmail.com>
2023-06-16 21:45:00 +0300

    fix

--- app.js
+++ app.js
@@ -9,8 +9,12 @@ async function CorotosGetData(search){
         ignoreDefaultArgs: ['--disable-extensions'],
         args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage'],
         //executablePath: '/usr/bin/chromium-browser'
+        
     });
     const page = await browser.newPage();
+    await page.setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36")
+
+
     await page.goto(url);
     const bookData = await page.evaluate(() => {
 
